672b47a08cbfafc1c0486f2252f4dc4e27e86ded
hale <59288230+halekmc@users.noreply.github.com>
2022-07-06 13:42:06 +0300

    made project

---
+++ ForeksCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForeksCase
+//
+//  Created by KBM-PC on 6.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
